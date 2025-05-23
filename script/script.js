60a129d3dc710d5ac7a83d4e8d786179aff7a394
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-05-23 18:28:09 +0900

    퍼블리싱 중

--- script/script.js
+++ script/script.js
@@ -8,5 +8,25 @@ const slideSec_slide = () => {
     var swiper = new Swiper(".watermelon .slideSec .slideBox .mySwiper", {
         slidesPerView: 5,
         spaceBetween: 20,
+        centeredSlides: true,
+        loop : true,
+        autoplay: {
+            delay: 1500,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            640: {
+              slidesPerView: 1.4,
+              spaceBetween: 5,
+            },
+            950: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+            1520: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+        },
     });
 }
