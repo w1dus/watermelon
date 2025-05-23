50e4ddd046422dbef47371e6b787f6b4d47babbf
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-05-23 14:03:39 +0900

    흑수박 퍼블리싱 시작

--- script/script.js
+++ script/script.js
@@ -1,7 +1,12 @@
 
 
 document.addEventListener("DOMContentLoaded", function(e){
-
+    slideSec_slide()
 })
 
-
+const slideSec_slide = () => {
+    var swiper = new Swiper(".watermelon .slideSec .slideBox .mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 20,
+    });
+}
